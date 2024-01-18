219697118d3d8a250bac5e8c3fca9d170f98c327
assrat007 <asratadane169@gmail.com>
2024-01-18 12:05:36 +0300

    final look

---
+++ src/components/home/popular-course.data.ts
@@ -0,0 +1,59 @@
+import type { Course } from '@/interfaces/course'
+
+export const data: Array<Course> = [
+  {
+    id: 1,
+    cover: '/images/courses/a9e7b27a0c5e986a22416d79e2e9dba9.jpg',
+    title: 'Website Developemnt from Zeo to Hero',
+    rating: 5,
+    ratingCount: 8,
+    price: 499,
+    category: 'Beginner',
+  },
+  {
+    id: 2,
+    cover: '/images/courses/alvaro-reyes-qWwpHwip31M-unsplash.jpg',
+    title: 'UI/UX Complete Guide',
+    rating: 5,
+    ratingCount: 15,
+    price: 499,
+    category: 'Intermediate',
+  },
+  {
+    id: 3,
+    cover: '/images/courses/christopher-gower-m_HRfLhgABo-unsplash.jpg',
+    title: 'Python Programing ',
+    rating: 4,
+    ratingCount: 7,
+    price: 599,
+    category: 'Beginner',
+  },
+  {
+    id: 4,
+    cover: '/images/courses/true-agency-o4UhdLv5jbQ-unsplash.jpg',
+    title: 'Mobile Application Developemnt Basics',
+    rating: 4,
+    ratingCount: 12,
+    price: 599,
+    category: 'Intermediate',
+  },
+  {
+    id: 5,
+    cover: '/images/courses/stillness-inmotion-Jh6aQX-25Uo-unsplash.jpg',
+    title: 'Modern React Website Developemnt',
+    rating: 4,
+    ratingCount: 32,
+    price: 599,
+    category: 'Intermediate',
+  },
+
+  {
+    id: 7,
+    cover: '/images/courses/grovemade-RvPDe41lYBA-unsplash.jpg',
+    title: 'Adobe Photoshop For Beginners: Complete Photo Editing',
+    rating: 4,
+    ratingCount: 6,
+    price: 599,
+    category: 'Beginner',
+  },
+]
